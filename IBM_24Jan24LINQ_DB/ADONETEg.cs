c970820bff9a0dffea712984d06d31d5c7a2d02e
EZLLeno <EZLLeno@EZL-LEN>
2024-01-29 13:16:49 +0530

    Dataset

--- IBM_24Jan24LINQ_DB/ADONETEg.cs
+++ IBM_24Jan24LINQ_DB/ADONETEg.cs
@@ -63,13 +63,162 @@ namespace IBM_24Jan24LINQ_DB
                 }
                 if (choice == "5")
                 {
-                   
+                    CallStoredProcEg(_cn);
+
+                }
+ if (choice == "6")
+                {
+                    DatasetDataTableDisconnectedArcDataAdap(_cn);
 
                 }
 
             } while (choice != "0");
 
 
+
+        }
+
+        private static void DatasetDataTableDisconnectedArcDataAdap(SqlConnection _cn)
+        {
+           
+
+            SqlDataAdapter _da= new SqlDataAdapter ();
+
+            _da.SelectCommand = new SqlCommand("select * from employee", _cn);
+
+            DataTable _dt = new DataTable();
+            DataSet _ds = new DataSet();
+
+            _da.Fill(_dt);
+            _da.Fill(_ds);
+            Console.Clear();
+            Console.WriteLine($"EmpID          |EmpName          |Salary");
+
+
+            foreach (DataRow _dr in _dt.Rows)
+            {
+                Console.WriteLine($"{_dr[0]}|{_dr[1]}|{_dr[2]}");
+
+
+            }
+            
+            foreach (DataRow _dr in _ds.Tables[0].Rows)
+            {
+                Console.WriteLine($"{_dr[0]}|{_dr[1]}|{_dr[2]}");
+
+
+            }
+
+
+            DataRow newDr= _dt.NewRow();
+
+
+            newDr[1] = "Ramesh";
+            newDr[2] = 1234;
+
+
+            _dt.Rows.Add(newDr);
+
+
+            DataRow _EditDR = _dt.Select("EmployeeID=1002")[0];
+
+            _EditDR[1] = "Dinesh Suresh";
+            _EditDR[2] = 500;
+
+
+            DataRow _DeleteDR = _dt.Select("EmployeeID=2")[0];
+            _DeleteDR.Delete();
+
+
+            _da.InsertCommand = new
+                SqlCommand(cmdText: "INSERT INTO [Employee] ( [EmpName], [Salary]) VALUES ( @empnm,@sal )",
+                connection: _cn);
+
+            _da.InsertCommand.Parameters.Add("@empnm", SqlDbType.VarChar, 50).SourceColumn= "EmpName";
+            _da.InsertCommand.Parameters.Add("@sal", SqlDbType.Money).SourceColumn = "Salary";
+
+
+
+            _da.UpdateCommand = new
+                SqlCommand(cmdText: "update  [Employee] set  [EmpName]=@empnm, [Salary] =@sal where EmployeeID=@eid",
+                connection: _cn);
+
+            _da.UpdateCommand.Parameters.Add("@empnm", SqlDbType.VarChar, 50).SourceColumn = "EmpName";
+            _da.UpdateCommand.Parameters.Add("@sal", SqlDbType.Money).SourceColumn = "Salary";
+            _da.UpdateCommand.Parameters.Add("@eid", SqlDbType.Int).SourceColumn = "EmployeeID";
+
+            _da.DeleteCommand = new
+                SqlCommand(cmdText: "delete  [Employee]  where EmployeeID=@eid",
+                connection: _cn);
+
+             _da.DeleteCommand.Parameters.Add("@eid", SqlDbType.Int).SourceColumn = "EmployeeID";
+
+            _da.ContinueUpdateOnError = true;
+
+            _da.Update(_dt);
+
+        }
+
+        private static void CallStoredProcEg(SqlConnection _cn)
+        {
+            Console.WriteLine("\n\nStored Proc ");
+            Console.Write("Emp ID to Seek:");
+            string empID = Console.ReadLine();
+        
+
+
+            SqlCommand _cmd = new
+                SqlCommand(cmdText: "sp_GetEmpNameByID",
+                connection: _cn);
+
+            _cmd.CommandType = CommandType.StoredProcedure ;
+
+
+          SqlParameter prm=  _cmd.Parameters.Add("@empName", SqlDbType.VarChar, 50);
+
+            prm.Direction = ParameterDirection.Output;
+
+
+            _cmd.Parameters.Add("@empid", SqlDbType.Int).Value = empID;
+
+
+            _cn.Open();
+
+            _cmd.ExecuteNonQuery(); 
+                Console.WriteLine($"EmpName is {prm.Value} for Emp Id {empID}");
+                Console.WriteLine("Press any key to Continue...");
+
+            _cmd = new SqlCommand("sp_GetEmployees", _cn);
+            _cmd.CommandType= CommandType.StoredProcedure ;
+
+            SqlDataReader _drd = _cmd.ExecuteReader();
+
+            if (_drd.HasRows == false)
+            {
+                Console.Clear(); Console.Write("Record(s) not found!!!\nPress any key to Continue...");
+                Console.ReadKey();
+                return;
+            }
+            Console.Clear();
+            Console.WriteLine($"EmpID          |EmpName          |Salary");
+            while (_drd.Read())
+            {
+                Console.WriteLine($"{_drd.GetValue(0)}|{_drd.GetValue(1)}|{_drd.GetValue(2)}");
+            }
+            Console.Write("Press any key to Continue...");
+            Console.ReadKey();
+
+            _drd.Close(); _cn.Close();
+
+
+
+
+
+
+
+            _cn.Close();
+
+            Console.ReadKey();
 
         }
 
